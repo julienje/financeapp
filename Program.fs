f59e5f6684be92def994170cde4738544d405ba4
Julien Jeanneret <julien.jeanneret@outlook.com>
2022-05-29 23:08:03 +0200

    feat: try to add mongo db

--- Program.fs
+++ Program.fs
@@ -1,18 +1,10 @@
 namespace FinanceApp
+
 #nowarn "20"
-open System
-open System.Collections.Generic
-open System.IO
-open System.Linq
-open System.Threading.Tasks
-open Microsoft.AspNetCore
+
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
 
 module Program =
     let exitCode = 0
@@ -20,7 +12,8 @@ module Program =
     [<EntryPoint>]
     let main args =
 
-        let builder = WebApplication.CreateBuilder(args)
+        let builder =
+            WebApplication.CreateBuilder(args)
 
         builder.Services.AddControllers()
 
---
+++ mongodb/MongoDB.fs
@@ -0,0 +1,15 @@
+﻿module FinanceApp.mongodb.MongoDB
+
+open MongoDB.Driver
+open System
+
+let readAll =
+    let connectionString = @"mongodb://localhost:27017"
+    let client = new MongoClient(connectionString)
+    let database = client.GetDatabase("financeDB")
+    use names = client.ListDatabaseNames()
+    while names.MoveNext() do
+        let join = String.Join(" ", names.Current)
+        printfn $"Name is %s{join}"
+    let dbs = client.ListDatabaseNames
+    2
