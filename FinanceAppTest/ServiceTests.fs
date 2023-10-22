78dab9deef1576bcf730b38ecdfa88a009a657b6
u231154 <julien.jeanneret2@sbb.ch>
2023-10-22 11:44:26 +0200

    feat: add test to implements trend

--- FinanceAppTest/ServiceTests.fs
+++ FinanceAppTest/ServiceTests.fs
@@ -6,40 +6,44 @@ open FinanceApp.DbType
 open MongoDB.Bson
 open Xunit
 
-let accountAId = "3231fc4a-3297-4ae4-85d5-3163147e0e1c"
-let accountBId ="00db8520-70b9-11ee-b962-0242ac120002"
+let accountAId = ObjectId.GenerateNewId()
+let accountBId = ObjectId.GenerateNewId()
 
-let createAccount (id, name) =
-    {
-        _id= ObjectId.Parse(id)
-        Name= name
-        Company= "My Company"
-        OpenDate= DateTime.MinValue
-        CloseDate=  Nullable() }
+let createAccount (id, name, closeDate) =
+    { _id = id
+      Name = name
+      Company = "My Company"
+      OpenDate = DateTime.MinValue
+      CloseDate = closeDate }
+
+let createBalance (accountId, datetime, amount) =
+    { _id = ObjectId.GenerateNewId()
+      AccountId = accountId
+      CheckDate = datetime
+      AmountInChf = amount }
+
+let dateToDateTime (date) = DateTime.Parse(date + " 12:00:00")
 
 let mockedAllAccounts: GetAllDbAccount =
- fun () ->
+    fun () ->
         task {
-            let accountA = createAccount(accountAId, "A")
-            let accountB = createAccount(accountBId, "B")
-            return [accountA; accountB]
+            let closeDate = dateToDateTime ("2023-06-01")
+            let accountA = createAccount (accountAId, "A", Nullable())
+            let accountB = createAccount (accountBId, "B", Nullable closeDate)
+            return [ accountA; accountB ]
         }
-let mockedAllBalances : GetAllDbBalances =
-     fun () ->
-        task {
-            let balance= {
-                  _id= ObjectId.Parse(Guid.NewGuid().ToString())
-                  AccountId= ObjectId.Parse(accountAId)
-                  CheckDate= DateTime.Now
-                  AmountInChf= 12.0m
-            }
-            return [balance]
-        }
+
+let mockedAllBalances: GetAllDbBalances =
+    fun () -> task { return [
+        createBalance (accountAId, dateToDateTime ("2023-01-27"), 12.0M);
+        createBalance (accountAId, dateToDateTime ("2023-02-27"), 12.0M);
+        createBalance (accountAId, dateToDateTime ("2023-03-27"), 12.0M)
+    ] }
 
 [<Fact>]
 let ``My test`` () =
-    let resp = task {
-        return! Service.handleGetTrendsAsync mockedAllAccounts mockedAllBalances
-    }
+    let respAsync = Service.handleGetTrendsAsync mockedAllAccounts mockedAllBalances
+    let resp = respAsync.Wait()
+
     Assert.True(true)
-    Assert.NotNull(resp)
+    Assert.NotNull(respAsync)
