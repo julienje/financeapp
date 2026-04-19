56b75d75cc46c4f137455ff557fc520c2da7ff51
Julien <julien.jeanneret@outlook.com>
2026-04-19 19:04:47 +0200

    feat: use plugin react instead of react swc and compare to a new project (#553)
    * feat: use plugin react instead of react swc and compare to a new project
    
    * chore: generate package-lock again

--- FinanceWeb/vite.config.ts
+++ FinanceWeb/vite.config.ts
@@ -1,12 +1,9 @@
 import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    resolve: {
-        conditions: ['mui-modern', 'module', 'browser', 'development|production']
-    },
     server: {
         proxy: {
             '/api': 'http://localhost:5165'
